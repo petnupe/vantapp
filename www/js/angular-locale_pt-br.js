e047885da04913df0dfffd7c76003e478a003f78
Server Test <gustavo@tecbiz.com.br>
2016-03-17 11:07:38 -0300

    mais uma

--- www/js/angular-locale_pt-br.js
+++ www/js/angular-locale_pt-br.js
@@ -1,3 +1,4 @@
+// By Peterson
 'use strict';
 angular.module("ngLocale", [], ["$provide", function($provide) {
 var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
